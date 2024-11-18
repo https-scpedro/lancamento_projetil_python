9f4e3cd7eba375537a8126825957516e99a6d5ea
JaoVicy <filipimjoa@gmail.com>
2024-11-18 14:34:38 -0300

    Finalizacao completa

--- main.py
+++ main.py
@@ -39,3 +39,33 @@ except Exception as e:
     print(f"Erro ao calcular {e}")
     exit(1)
 
+# Criando o Gráfico:
+plt.figure(figsize = (6, 10))
+plt.plot(x_values, y_values, label = "Tragetória de um Projétil", color = "green")
+
+# Marcações de H_max e d_max:
+plt.scatter([d_maxima / 2, d_maxima], [h_maxima, 0], color="blue", label="Pontos principais (H_max e d_max)")
+plt.axhline(h_maxima, color = "blue", linestyle = "--", linewidth = 0.7)
+plt.axvline(d_maxima, color = "blue", linestyle = "--", linewidth = 0.7)
+
+# Vetores de Velocidade:
+for i in range(len(pontos_tempo)):
+    plt.quiver(
+        pontos_x[i], pontos_y[i], velocidades_vx[i], velocidades_vy[i],
+        angles = "xy", scale_units = "xy", scale = 1, color = "red", label = "Velocidade (v)"
+
+        if i == 0 
+        else ""
+    )
+
+#Configurações do Gráfico:
+plt.title("Lançamento de Projéteis")
+plt.xlabel("Posisão Horizontal (m)")
+plt.ylabel("Posição Vertical (m)")
+plt.axhline(0, color = "black", linewidth = 0.8)
+plt.axvline(0, color = "black", linewidth = 0.8)
+plt.legend()
+plt.grid()
+
+plt.show()
+
